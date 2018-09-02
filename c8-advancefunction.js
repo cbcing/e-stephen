8359399786d92b926e31bd9064f81747086045d9
David <davvvvvvidchen@gmail.com>
2018-09-02 12:07:38 +0800

    高阶函数示例2

--- c8-advancefunction.js
+++ c8-advancefunction.js
@@ -1,6 +1,6 @@
 
 // 高阶函数：操作函数的函数，接收一个或多个函数作为参数，并返回一个新函数
-
+// 示例1
 function not (f) {
     return function () {
         var result = f.apply(this, arguments);
@@ -15,3 +15,21 @@ var even = function (x) {
 var odd = not(even);
 
 console.log([1, 1, 3, 5, 5].every(odd));    // true
+
+// 示例2
+function compose(f, g) {
+    return function () {
+        return f.call(this, g.apply(this, arguments));
+    }
+}
+
+var square = function (x) {
+    return x * x;
+}
+
+var sum = function (x, y) {
+    return x + y;
+}
+
+var squareofsum = compose(square, sum);
+console.log(squareofsum(2, 3)); // (2+3)*5=25
